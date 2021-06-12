94b684cde46c6e44eba3a4e88c48189bfa3091bb
Neelam <neelamchaubey28@gmail.com>
2021-06-12 20:30:55 +0530

    completed banner section

---
+++ assets/js/custom.js
@@ -0,0 +1,254 @@
+jQuery(document).ready(function ($) {
+
+
+    "use strict";
+
+
+
+    $(function () {
+        $("#tabs").tabs();
+    });
+
+
+    // Page loading animation
+
+    $("#preloader").animate({
+        'opacity': '0'
+    }, 600, function () {
+        setTimeout(function () {
+            $("#preloader").css("visibility", "hidden").fadeOut();
+        }, 300);
+    });
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var box = $('.header-text').height();
+        var header = $('header').height();
+
+        if (scroll >= box - header) {
+            $("header").addClass("background-header");
+        } else {
+            $("header").removeClass("background-header");
+        }
+    });
+    if ($('.owl-testimonials').length) {
+        $('.owl-testimonials').owlCarousel({
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            margin: 30,
+            autoplay: false,
+            smartSpeed: 700,
+            autoplayTimeout: 6000,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                460: {
+                    items: 1,
+                    margin: 0
+                },
+                576: {
+                    items: 2,
+                    margin: 20
+                },
+                992: {
+                    items: 2,
+                    margin: 30
+                }
+            }
+        });
+    }
+    if ($('.owl-partners').length) {
+        $('.owl-partners').owlCarousel({
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            margin: 30,
+            autoplay: false,
+            smartSpeed: 700,
+            autoplayTimeout: 6000,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                460: {
+                    items: 1,
+                    margin: 0
+                },
+                576: {
+                    items: 2,
+                    margin: 20
+                },
+                992: {
+                    items: 4,
+                    margin: 30
+                }
+            }
+        });
+    }
+
+    $(".Modern-Slider").slick({
+        autoplay: true,
+        autoplaySpeed: 10000,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        dots: true,
+        pauseOnDotsHover: true,
+        cssEase: 'linear',
+        // fade:true,
+        draggable: false,
+        prevArrow: '<button class="PrevArrow"></button>',
+        nextArrow: '<button class="NextArrow"></button>',
+    });
+
+    function visible(partial) {
+        var $t = partial,
+            $w = jQuery(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
+
+    }
+
+    $(window).scroll(function () {
+
+        if (visible($('.count-digit'))) {
+            if ($('.count-digit').hasClass('counter-loaded')) return;
+            $('.count-digit').addClass('counter-loaded');
+
+            $('.count-digit').each(function () {
+                var $this = $(this);
+                jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 3000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+        }
+    })
+
+});
+$(document).ready(function () {
+    var itemsMainDiv = ('.MultiCarousel');
+    var itemsDiv = ('.MultiCarousel-inner');
+    var itemWidth = "";
+
+    $('.leftLst, .rightLst').click(function () {
+        var condition = $(this).hasClass("leftLst");
+        if (condition)
+            click(0, this);
+        else
+            click(1, this)
+    });
+
+    ResCarouselSize();
+
+
+
+
+    $(window).resize(function () {
+        ResCarouselSize();
+    });
+
+    //this function define the size of the items
+    function ResCarouselSize() {
+        var incno = 0;
+        var dataItems = ("data-items");
+        var itemClass = ('.item');
+        var id = 0;
+        var btnParentSb = '';
+        var itemsSplit = '';
+        var sampwidth = $(itemsMainDiv).width();
+        var bodyWidth = $('body').width();
+        $(itemsDiv).each(function () {
+            id = id + 1;
+            var itemNumbers = $(this).find(itemClass).length;
+            btnParentSb = $(this).parent().attr(dataItems);
+            itemsSplit = btnParentSb.split(',');
+            $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+            if (bodyWidth >= 1200) {
+                incno = itemsSplit[3];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 992) {
+                incno = itemsSplit[2];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 768) {
+                incno = itemsSplit[1];
+                itemWidth = sampwidth / incno;
+            }
+            else {
+                incno = itemsSplit[0];
+                itemWidth = sampwidth / incno;
+            }
+            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+            $(this).find(itemClass).each(function () {
+                $(this).outerWidth(itemWidth);
+            });
+
+            $(".leftLst").addClass("over");
+            $(".rightLst").removeClass("over");
+
+        });
+    }
+
+
+    //this function used to move the items
+    function ResCarousel(e, el, s) {
+        var leftBtn = ('.leftLst');
+        var rightBtn = ('.rightLst');
+        var translateXval = '';
+        var divStyle = $(el + ' ' + itemsDiv).css('transform');
+        var values = divStyle.match(/-?[\d\.]+/g);
+        var xds = Math.abs(values[4]);
+        if (e == 0) {
+            translateXval = parseInt(xds) - parseInt(itemWidth * s);
+            $(el + ' ' + rightBtn).removeClass("over");
+
+            if (translateXval <= itemWidth / 2) {
+                translateXval = 0;
+                $(el + ' ' + leftBtn).addClass("over");
+            }
+        }
+        else if (e == 1) {
+            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+            translateXval = parseInt(xds) + parseInt(itemWidth * s);
+            $(el + ' ' + leftBtn).removeClass("over");
+
+            if (translateXval >= itemsCondition - itemWidth / 2) {
+                translateXval = itemsCondition;
+                $(el + ' ' + rightBtn).addClass("over");
+            }
+        }
+        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+    }
+
+    //It is used to get some elements from btn
+    function click(ell, ee) {
+        var Parent = "#" + $(ee).parent().attr("id");
+        var slide = $(Parent).attr("data-slide");
+        ResCarousel(ell, Parent, slide);
+    }
+    // Services On HOver Animation Start
+    $('.has-dropdown').hover(function () {
+        $(this).toggleClass('open');
+    });
+    // Services On HOver Animation End
+});
---
+++ partials/header.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="TwoMadsMedia">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+    rel="stylesheet">
+
+    <title>Budget Friendly Digital Marketing Agency in Mumbai, Maharashtra</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+  <!--
+
+Finance Business TemplateMo
+
+https://templatemo.com/tm-545-finance-business
+
+-->
+</head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="preloader">
+    <div class="jumper">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-xs-12">
+          <ul class="left-info">
+            <li><a href="mailto:connect@twomadsmedia.com"><i class="fa fa-envelope-o"></i>connect@twomadsmedia.com</a></li>
+            <li><a href="tel:9637374782"><i class="fa fa-phone"></i>9637374782</a></li>
+            <li><a href="tel:9699906857"><i class="fa fa-mobile"></i>9699906857</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <ul class="right-icons">
+            <li class="social-links facebook"><a href="https://www.facebook.com/Twomads_media"><i class="fa fa-facebook"></i></a></li>
+            <li class="social-links instagram"><a href="https://www.instagram.com/twomads_media/"><i class="fa fa-instagram"></i></a></li>
+            <li class="social-links linkedin"><a href="https://www.linkedin.com/twomads_media/"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <header class="">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">
+          <img src="./assets/images/logo.svg" alt="Two Mads Media">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+          aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link current" href="#top">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="services.php">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contactus">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Page Content -->
+  <!-- Banner Starts Here -->
+  <div class="main-banner header-text" id="top">
+    <div class="Modern-Slider">
+      <!-- Item -->
+      <div class="item item-1">
+        <div class="img-fill">
+          <div class="text-content">
+            <h4>WELCOME TO <br>TWOMADS MEDIA</h4>
+            <p>Every Business Idea Needs an Appropriate Media to Prosper. <br>That’s What We Are!</p>
+            <a href="#features" class="scroll-mouse">
+              <div class="scroll-downs">
+                <div class="mousey">
+                  <div class="scroller"></div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- // Item -->
+    </div>
+  </div>
+  <!-- Banner Ends Here -->
